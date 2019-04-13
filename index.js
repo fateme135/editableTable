842359d7d703cf172b49abf7d6c3154b4c7f4249
fateme135 <m.fatemeh74@gmail.com>
2019-04-13 11:11:44 +0430

    delete

--- index.js
+++ index.js
@@ -7,67 +7,140 @@ let styles = {
         alignItems: 'center',
         height: '100%',
         width: '80%',
-        color :'green'
+        color: 'green'
+    },
+    table: {
+        width: '70%',
+        maxWidth: '900px',
+        textAlign: 'center',
+        background: '#FFF',
+        padding: '10px',
+        borderRadius: '10px',
+        boxShadow: '0 2px 5px rgba(0,0,0,0.2)'
+    },
+    input: {
+        borderTopWidth: 0,
+        borderLeftWidth: 0,
+        borderRightWidth: 0,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ff6f5f',
+        width: '100%'
     }
 }
 ///////////////////////////////////////////////////////
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { rows: [] }
-        this.submit = this.submit.bind(this);//chon az arrow fun estefade nakardim felan bind mikonim
+        this.state = {
+            rows: [],
+            firstName: '',
+            lastName: '',
+            age: 0,
+            sex: false
+        }
+        // this.submit = this.submit.bind(this);//chon az arrow fun estefade nakardim felan bind mikonim
+        this.onChange = this.onChange.bind(this);//bind ro ham mishe in ja kard ham to on jayi ke farakhani mikonim
     }
-
-    submit(e) {
-        e.preventDefault();
-        const { rows } = this.state;// === ba this.state.rows
-        const firstName = document.getElementById('firstName').value;
-        const lastName = document.getElementById('lastName').value;
-        const age = document.getElementById('age').value;
-        // console.log(age);
-        rows.push({ firstName, lastName, age })
-        this.setState({ rows })//ta zamani ke in ro nanevisim render nemikonad chon ta state taqir nakonad render farakhani nemishavad
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // submit(e) {
+    //     e.preventDefault();
+    //     const { rows } = this.state;// === ba this.state.rows
+    //     const firstName = document.getElementById('firstName').value;
+    //     const lastName = document.getElementById('lastName').value;
+    //     const age = document.getElementById('age').value;
+    //     // console.log(age);
+    //     rows.push({ firstName, lastName, age })
+    //     this.setState({ rows })//ta zamani ke in ro nanevisim render nemikonad chon ta state taqir nakonad render farakhani nemishavad
+    // }
+    ////////////////////////===onsubmit/////////////////////////////////////////////////
+    addNew() {
+        let { rows, firstName, lastName, age, sex } = this.state;
+        rows.push({ firstName, lastName, age, sex, id:uuidv4() });
+        this.setState({ rows });
     }
-
+    ////////////////////////////////////////////////////////////////////////////
+    onChange(event) {
+        let { name, value, type, checked } = event.target;
+        if (type === "checkbox") {
+            this.setState({ [name]: checked })
+        }
+        else {
+            this.setState({ [name]: value })
+        }
+    }
+    ////////////////////////////////First Solution with index for delete it is no nice////////////////////////////////////////////
+    ondelete(index) {
+        const { rows } = this.state;
+        rows.splice(index, 1);
+        this.setState({ rows });
+    }
+    ///////////////////////////////////second solution/////////////////////////////////////////
+    // ondelete(id) {
+        
+    //     let { rows } = this.state;
+    //     rows = rows.filter(value => value.id !== id);
+    //     this.setState({ rows });
+    //     debugger;
+    // }
+    ////////////////////////////////////////////////////////////////////////////
     render() {
-        const { rows } = this.state;// === ba this.state.rows
+        // const { rows } = this.state;// === ba this.state.rows
+        let { rows, firstName, lastName, age, sex } = this.state;
         return (
             <div>
                 <h1 className="title"> table</h1>
-                <form onSubmit={this.submit}>
+                {/* <form onSubmit={this.submit}>
                     <input type="text" id='firstName' />
                     <input type="text" id='lastName' />
                     <input type="number" id='age' />
                     <button type="submit">add</button>
-                </form>
+                </form> */}
                 <table className="table">
                     <thead className="table">
-                     <tr>
-                        <th>firstName</th>
-                        <th> lastName  </th>
-                        <th> age </th>
-                    </tr>
-                 </thead>
-                 <tbody className="container">
-                     {
-                         rows.map((row, index) =>
-                          {//alan ma map zadim ke yeseri row ro map konim be tr
-                         return (
-                                 //agar in ro nazanim moshkeli pish nemiomad faqad ye warning midad va in ba khater in ke react 
-                                 <tr key={index}> 
-                                <td>{row.firstName}</td>
-                                <td>{row.lastName}</td>
-                                <td>{row.age}</td>
-                                </tr>
+                        <tr>
+                            <th>firstName</th>
+                            <th> lastName  </th>
+                            <th> age </th>
+                            <th> sex </th>
+                            <th> action </th>
+                        </tr>
+                        <tr>
+                        <td><input defaultValue={firstName} name="firstName" style={styles.input} type="text" onChange={this.onChange} /></td>
+                            <td><input defaultValue={lastName} name="lastName" style={styles.input} type="text" onChange={this.onChange} /></td>
+                            <td><input defaultValue={age} name="age" style={styles.input} type="number" onChange={this.onChange} /></td>
+                            <td><input checked={sex} name="sex" style={styles.input} type="checkbox" onChange={this.onChange} /></td>
+                            <td><button onClick={this.addNew.bind(this)}>Add</button></td>
+
+                        </tr>
+                    </thead>
+                    <tbody className="container">
+                        {
+                            rows.map((row, index) => {//alan ma map zadim ke yeseri row ro map konim be tr
+                                return (
+                                    //agar in ro nazanim moshkeli pish nemiomad faqad ye warning midad va in ba khater in ke react 
+                                    <tr key={row.id}>
+
+                                        <td>{row.firstName}</td>
+                                        <td>{row.lastName}</td>
+                                        <td>{row.age}</td>
+                                        <td>{row.sex}</td>
+                                        <td> <button onClick={this.ondelete.bind(this)}>Delete</button></td>
+                                    </tr>
                                 )
                             })
-                     }
+                        }
                     </tbody>
                 </table>
             </div>)
     }
 }
 ///////////////////////////////////////////////////////
-
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+/////////////////////////////////////////////////////////////
 const root = document.getElementById('root')
 ReactDOM.render(<App />, root) 
--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -17,6 +19,23 @@ var styles = {
         height: '100%',
         width: '80%',
         color: 'green'
+    },
+    table: {
+        width: '70%',
+        maxWidth: '900px',
+        textAlign: 'center',
+        background: '#FFF',
+        padding: '10px',
+        borderRadius: '10px',
+        boxShadow: '0 2px 5px rgba(0,0,0,0.2)'
+    },
+    input: {
+        borderTopWidth: 0,
+        borderLeftWidth: 0,
+        borderRightWidth: 0,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ff6f5f',
+        width: '100%'
     }
     ///////////////////////////////////////////////////////
 };
@@ -28,28 +47,92 @@ var App = function (_React$Component) {
 
         var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
-        _this.state = { rows: [] };
-        _this.submit = _this.submit.bind(_this); //?????????????
+        _this.state = {
+            rows: [],
+            firstName: '',
+            lastName: '',
+            age: 0,
+            sex: false
+            // this.submit = this.submit.bind(this);//chon az arrow fun estefade nakardim felan bind mikonim
+        };_this.onChange = _this.onChange.bind(_this); //bind ro ham mishe in ja kard ham to on jayi ke farakhani mikonim
         return _this;
     }
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // submit(e) {
+    //     e.preventDefault();
+    //     const { rows } = this.state;// === ba this.state.rows
+    //     const firstName = document.getElementById('firstName').value;
+    //     const lastName = document.getElementById('lastName').value;
+    //     const age = document.getElementById('age').value;
+    //     // console.log(age);
+    //     rows.push({ firstName, lastName, age })
+    //     this.setState({ rows })//ta zamani ke in ro nanevisim render nemikonad chon ta state taqir nakonad render farakhani nemishavad
+    // }
+    ////////////////////////===onsubmit/////////////////////////////////////////////////
+
 
     _createClass(App, [{
-        key: 'submit',
-        value: function submit(e) {
-            e.preventDefault();
-            var rows = this.state.rows; // === ba this.state.rows
+        key: 'addNew',
+        value: function addNew() {
+            var _state = this.state,
+                rows = _state.rows,
+                firstName = _state.firstName,
+                lastName = _state.lastName,
+                age = _state.age,
+                sex = _state.sex;
 
-            var firstName = document.getElementById('firstName').value;
-            var lastName = document.getElementById('lastName').value;
-            var age = document.getElementById('age').value;
-            // console.log(age);
-            rows.push({ firstName: firstName, lastName: lastName, age: age });
-            this.setState({ rows: rows }); //ta zamani ke in ro nanevisim render nemikonad chon ta state taqir nakonad render farakhani nemishavad
+            rows.push({ firstName: firstName, lastName: lastName, age: age, sex: sex, id: uuidv4() });
+            this.setState({ rows: rows });
         }
+        ////////////////////////////////////////////////////////////////////////////
+
+    }, {
+        key: 'onChange',
+        value: function onChange(event) {
+            var _event$target = event.target,
+                name = _event$target.name,
+                value = _event$target.value,
+                type = _event$target.type,
+                checked = _event$target.checked;
+
+            if (type === "checkbox") {
+                this.setState(_defineProperty({}, name, checked));
+            } else {
+                this.setState(_defineProperty({}, name, value));
+            }
+        }
+        ////////////////////////////////First Solution with index for delete it is no nice////////////////////////////////////////////
+
+    }, {
+        key: 'ondelete',
+        value: function ondelete(index) {
+            var rows = this.state.rows;
+
+            rows.splice(index, 1);
+            this.setState({ rows: rows });
+        }
+        ///////////////////////////////////second solution/////////////////////////////////////////
+        // ondelete(id) {
+
+        //     let { rows } = this.state;
+        //     rows = rows.filter(value => value.id !== id);
+        //     this.setState({ rows });
+        //     debugger;
+        // }
+        ////////////////////////////////////////////////////////////////////////////
+
     }, {
         key: 'render',
         value: function render() {
-            var rows = this.state.rows; // === ba this.state.rows
+            var _this2 = this;
+
+            // const { rows } = this.state;// === ba this.state.rows
+            var _state2 = this.state,
+                rows = _state2.rows,
+                firstName = _state2.firstName,
+                lastName = _state2.lastName,
+                age = _state2.age,
+                sex = _state2.sex;
 
             return React.createElement(
                 'div',
@@ -58,18 +141,6 @@ var App = function (_React$Component) {
                     'h1',
                     { className: 'title' },
                     ' table'
-                ),
-                React.createElement(
-                    'form',
-                    { onSubmit: this.submit },
-                    React.createElement('input', { type: 'text', id: 'firstName' }),
-                    React.createElement('input', { type: 'text', id: 'lastName' }),
-                    React.createElement('input', { type: 'number', id: 'age' }),
-                    React.createElement(
-                        'button',
-                        { type: 'submit' },
-                        'add'
-                    )
                 ),
                 React.createElement(
                     'table',
@@ -94,6 +165,49 @@ var App = function (_React$Component) {
                                 'th',
                                 null,
                                 ' age '
+                            ),
+                            React.createElement(
+                                'th',
+                                null,
+                                ' sex '
+                            ),
+                            React.createElement(
+                                'th',
+                                null,
+                                ' action '
+                            )
+                        ),
+                        React.createElement(
+                            'tr',
+                            null,
+                            React.createElement(
+                                'td',
+                                null,
+                                React.createElement('input', { defaultValue: firstName, name: 'firstName', style: styles.input, type: 'text', onChange: this.onChange })
+                            ),
+                            React.createElement(
+                                'td',
+                                null,
+                                React.createElement('input', { defaultValue: lastName, name: 'lastName', style: styles.input, type: 'text', onChange: this.onChange })
+                            ),
+                            React.createElement(
+                                'td',
+                                null,
+                                React.createElement('input', { defaultValue: age, name: 'age', style: styles.input, type: 'number', onChange: this.onChange })
+                            ),
+                            React.createElement(
+                                'td',
+                                null,
+                                React.createElement('input', { checked: sex, name: 'sex', style: styles.input, type: 'checkbox', onChange: this.onChange })
+                            ),
+                            React.createElement(
+                                'td',
+                                null,
+                                React.createElement(
+                                    'button',
+                                    { onClick: this.addNew.bind(this) },
+                                    'Add'
+                                )
                             )
                         )
                     ),
@@ -106,7 +220,7 @@ var App = function (_React$Component) {
                                 //agar in ro nazanim moshkeli pish nemiomad faqad ye warning midad va in ba khater in ke react 
                                 React.createElement(
                                     'tr',
-                                    { key: index },
+                                    { key: row.id },
                                     React.createElement(
                                         'td',
                                         null,
@@ -121,6 +235,21 @@ var App = function (_React$Component) {
                                         'td',
                                         null,
                                         row.age
+                                    ),
+                                    React.createElement(
+                                        'td',
+                                        null,
+                                        row.sex
+                                    ),
+                                    React.createElement(
+                                        'td',
+                                        null,
+                                        ' ',
+                                        React.createElement(
+                                            'button',
+                                            { onClick: _this2.ondelete.bind(_this2) },
+                                            'Delete'
+                                        )
                                     )
                                 )
                             );
@@ -135,5 +264,14 @@ var App = function (_React$Component) {
 }(React.Component);
 ///////////////////////////////////////////////////////
 
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : r & 0x3 | 0x8;
+        return v.toString(16);
+    });
+}
+/////////////////////////////////////////////////////////////
 var root = document.getElementById('root');
 ReactDOM.render(React.createElement(App, null), root);
